b89276ddd9ed59a22b5d895735fe85d5a7550528
Pro-Code-art <jayme.coder@gmail.com>
2021-04-11 19:56:42 -0700

    made successful api call to google books api

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,26 +1,46 @@
-import React, {useState, useEffect} from "react";
-import Form from "../../components/Form/Form"
+import React, { useState, useEffect } from "react";
+import Form from "../../components/Form/Form";
+import 'dotenv/config';
 
 function Search() {
-    const [book, setBook] = useState("");
+  const [books, setBooks] = useState("");
 
-    const handleSearch = () => {
-        fetch("api/books", {
-            method: "POST",
-            body: JSON.stringify({ book }),
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-        })
-            // .then(data => triggerReUpload(data))
-            .catch(err => alert(err));
-    };
-
-
-    return (    
-        <Form placeholder="Search for a book!" buttonName="search" onClick={handleSearch}/>
+  useEffect(() => {
+    // Google Books API call.
+    const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=pride+prejudice&download=epub&key=${apiKey}`
     )
+      .then((response) => console.log(response))
+    //   .then((result) => {
+    //     this.setBooks({ books: result.items });
+    //   });
+  }, []);
+
+  const handleSearch = () => {
+    console.log("our books are: ", books);
+  }
+
+  const saveBook = () => {
+    fetch("api/books", {
+      method: "POST",
+      body: JSON.stringify({ books }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      // .then(data => triggerReUpload(data))
+      .catch((err) => alert(err));
+  };
+
+  return (
+    <Form
+      placeholder="Search for a book!"
+      buttonName="search"
+      onClick={handleSearch}
+    />
+  );
 }
 
 export default Search;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require('morgan')
 const path = require("path");
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const BooksApiRoutes = require('./routes/BooksApi');
 // Define middleware here
