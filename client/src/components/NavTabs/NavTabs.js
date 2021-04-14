14f542c746af96e890481cc50ef7219b3366c680
Pro-Code-art <jayme.coder@gmail.com>
2021-04-13 21:36:38 -0700

    Router is now functional

--- client/src/components/NavTabs/NavTabs.js
+++ client/src/components/NavTabs/NavTabs.js
@@ -1,21 +1,14 @@
 import React from "react";
-import { BrowserRouter as Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function NavTabs() {
-  // const location = useLocation();
 
   return (
     <div>
-      <Link
-        to="/"
-        //   className={location.pathname === "/" ? "nav-link active" : "nav-link"}
-      >
+      <Link to="/">
         Find Book
       </Link>
-      <Link
-        to="/library"
-        // className={location.pathname === "/login" ? "nav-link active" : "nav-link"}
-      >
+      <Link to="/library">
         Library
       </Link>
     </div>
