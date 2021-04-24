cf99acf9270da43247803c9ec88d8705dc2fc819
Pro-Code-art <jayme.coder@gmail.com>
2021-04-24 01:10:20 -0700

    changed schema to be an array of objects, each containing book info

--- models/Book.js
+++ models/Book.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const BookSchema = new Schema({
+const BookSchema = new Schema([{
     title: { type: String },
     image: {type: String},
     author: { type: Array },
     description: {type: String},
     link: {type: String}
-});
+}]);
 
 const Book = mongoose.model('Book', BookSchema);
 
