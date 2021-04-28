0b9bdb33bf26756dfae19bd621a9730d32f68a84
Pro-Code-art <jayme.coder@gmail.com>
2021-04-27 22:25:05 -0700

    added styling to NavTabs and to Library.js

--- client/src/components/NavTabs/NavTabs.js
+++ client/src/components/NavTabs/NavTabs.js
@@ -3,6 +3,7 @@ import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { Link } from "react-router-dom";
+import "./NavTabs.css";
 
 export default function DisabledTabs() {
   const [value, setValue] = React.useState(2);
--- client/src/pages/Library/Library.js
+++ client/src/pages/Library/Library.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import BookCard from "../../components/BookCard/BookCard";
 import axios from "axios";
+import "./Library.css";
 
 function Library() {
   const [userBooksState, setUserBooksState] = useState([]);
@@ -15,21 +16,23 @@ function Library() {
   const goHome = () => window.location.replace("/");
 
   return (
-    <div>
-      <h2>Saved Books</h2>
-      {userBooksState.map((book, i) => (
-        <BookCard
-          key={i}
-          index={i}
-          imageUrl={book.image}
-          title={book.title}
-          description={book.description}
-          preview={book.link}
-          author={book.author}
-          onClick={goHome}
-        />
-      ))}
-    </div>
+    <>
+      <h2 className="libraryTitle">Saved Books</h2>
+      <div className="bookCardsDiv">
+        {userBooksState.map((book, i) => (
+          <BookCard
+            key={i}
+            index={i}
+            imageUrl={book.image}
+            title={book.title}
+            description={book.description}
+            preview={book.link}
+            author={book.author}
+            onClick={goHome}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
